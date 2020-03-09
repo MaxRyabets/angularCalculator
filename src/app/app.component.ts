afe97cef69b534f59d23692d0892dca79f279423
Max Ryabets <maksaxel2@gmail.com>
2020-03-09 15:13:42 +0200

    Fix <-, CE and pow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +13,7 @@ export class AppComponent {
   public static BACKSPACE = '<-';
   operation = '';
   expression = [];
-  result = 0;
+  result: any = 0;
   temp = 0;
   numberFlag = false;
   queueOperations = [];
@@ -27,7 +26,10 @@ export class AppComponent {
   };
 
   private singleNumberOperation = {
-    '=': () => this.result,
+    '=': () => {
+      this.queueOperations = [];
+      return this.result;
+    },
     '+/-': () => (this.result * (-1)).toString(),
   };
 
@@ -39,12 +41,16 @@ export class AppComponent {
     CE: () => {
       const expLength = this.expression.length;
       if (expLength > 1) {
-        this.expression[expLength - 1] = '0';
-      } else {
-        this.expression[0] = '0';
-      }
-      if (expLength > 2) {
+        this.result = this.temp = 0;
+        this.expression.pop();
+      } else if (expLength === 1 && (this.operation === '=' || this.operation === AppComponent.CE || this.operation.length === 0)) {
         this.expression = [];
+        this.queueOperations = [];
+        this.result = this.temp = 0;
+      } else if (expLength > 2) {
+        this.expression = [];
+      } else if (expLength === 1 && (this.operation === '<-' || this.operation === 'CE')) {
+        this.result = this.temp = 0;
       }
     }
   };
@@ -54,41 +60,54 @@ export class AppComponent {
       const lastElemLength = this.expression.length - 1;
       const lastElemExp = this.expression[lastElemLength];
       if (this.expression.length !== 0) {
-        // if expression have big numbers for backspace last numbers, in current number
+      // if expression have big numbers for backspace last numbers, in current number
         if (lastElemExp.length === 1) {
           // if last element equal length 1, but expression have two numbers
-          if (this.expression.length === 2) {
-            this.expression[this.expression.length - 1] = '0';
+          if (this.expression.length === 2 || (this.expression.length === 1 &&
+            !this.queueOperations.includes('+')) &&
+            !this.queueOperations.includes('-') &&
+            !this.queueOperations.includes('/') &&
+            !this.queueOperations.includes('*')
+          ) {
+            this.expression.pop();
             this.numberFlag = true;
           } else {
-            this.expression = [];
+            if (this.operation.length === 0) {
+              this.expression = [];
+            } else if (this.expression.length !== 1) {
+                this.expression[lastElemLength] = lastElemExp.substring(0, lastElemExp.length - 1);
+            }
           }
         } else {
           // if number have "-"
           if (lastElemExp[0] === '-' && lastElemExp.length === 2) {
             this.expression[lastElemLength] = '0';
           } else {
-            this.expression[lastElemLength] = lastElemExp.substring(0, lastElemExp.length - 1);
+            if (this.expression.length !== 0 && this.queueOperations.length !== 0) {
+              if ((!this.queueOperations.includes('+') &&
+                !this.queueOperations.includes('-') &&
+                !this.queueOperations.includes('/') &&
+                !this.queueOperations.includes('*')) ||
+                this.expression.length !== 1) {
+                this.expression[lastElemLength] = lastElemExp.substring(0, lastElemExp.length - 1);
+              }
+            } else if (this.expression.length === 0) {
+              this.expression = [];
+            } else if (this.expression.length !== 0 && this.queueOperations.length === 0) {
+              if ((!this.queueOperations.includes('+') &&
+                !this.queueOperations.includes('-') &&
+                !this.queueOperations.includes('/') &&
+                !this.queueOperations.includes('*'))) {
+                this.numberFlag = true;
+                this.expression[lastElemLength] = lastElemExp.substring(0, lastElemExp.length - 1);
+              }
+            }
           }
         }
       }
     }
   };
 
-  addPoint(point: string) {
-    const expLength = this.expression[this.expression.length - 1];
-    if (!expLength.includes('.')) {
-      this.expression[this.expression.length - 1] = expLength + point;
-    } else if (expLength.includes('.') && this.operation.length === 0) {
-      this.result = this.expression[this.expression.length - 1];
-    } else {
-      if (this.expression.length === 1) {
-        this.expression.push('0.');
-      }
-      this.result = this.expression[this.expression.length - 1];
-      this.numberFlag = true;
-    }
-  }
   /**
    * Search operand CE, <-
    */
@@ -99,28 +118,50 @@ export class AppComponent {
         this.result = this.temp = 0;
       } else {
         this.result = this.temp = this.expression[this.expression.length - 1];
-        this.numberFlag = true;
+        if (this.expression.length !== 1) {
+          this.numberFlag = true;
+        }
       }
     } else if (operand in this.clearEntry) {
       this.clearEntry[operand]();
-      this.temp = this.expression[this.expression.length - 1];
-      if (this.expression.length === 1) {
-        this.expression.pop();
+    }
+  }
+
+  addPoint(point: string) {
+    if (this.expression.length === 0) {
+      this.expression.push('0');
+      this.numberFlag = true;
+    }
+    const expLength = this.expression[this.expression.length - 1];
+    if (!expLength.includes('.')) {
+      if (this.expression.length === 1 && this.numberFlag){
+        this.result = this.expression[this.expression.length - 1] = expLength + point;
+      } else if (this.expression.length === 1 && this.operation.length !== 0) {
+        this.expression.push('0.');
+        this.numberFlag = true;
+        this.result = this.temp = this.expression[this.expression.length - 1];
+      } else {
+        this.result = this.expression[this.expression.length - 1] = expLength + point;
       }
-      this.result = this.temp;
+    } else if (expLength.includes('.') && this.operation.length === 0) {
+    } else {
+      if (this.expression.length === 1 && !this.numberFlag) {
+        this.expression.push('0.');
+        this.result = this.temp = this.expression[this.expression.length - 1];
+        this.numberFlag = true;
+      }
     }
   }
 
   private expFirstPow(operand: string, expLength: number) {
-    this.temp = this.expression[expLength - 1] = this.powerOperation[operand](+this.expression[expLength - 1]);
-    this.calc();
-  }
-
-  private expFirstClear(expLength: number) {
-    if (expLength > 0) {
-      this.expression[expLength - 1] = '0';
-      this.temp = this.expression.pop();
+    if (this.expression.length === 1) {
+      this.expression.push('0');
+    } else {
+      this.temp = this.expression[expLength - 1] = this.powerOperation[operand](+this.expression[expLength - 1]);
     }
+    this.calc();
+    this.operation = '';
+    this.numberFlag = true;
   }
 
   unaryMinus(operand: string) {
@@ -144,11 +185,17 @@ export class AppComponent {
   }
 
   expQueueOperations(operand: string) {
-    this.queueOperations.push(operand);
-    this.operation = this.queueOperations.shift();
-    this.calc();
-    this.operation = this.queueOperations.shift();
-    this.queueOperations = [];
+    if (operand !== '<-') {
+      this.queueOperations.push(operand);
+      this.operation = this.queueOperations.shift();
+      this.calc();
+      this.operation = this.queueOperations.shift();
+      if (this.operation === '=') {
+        this.operation = '';
+        this.numberFlag = true;
+      }
+      this.queueOperations = [];
+    }
   }
 
   addOperation(operand: string) {
@@ -159,21 +206,31 @@ export class AppComponent {
     if (operand === AppComponent.POW && opLength !== 0) {
       this.expFirstPow(operand, expLength);
     } else if (operand === AppComponent.CE && opLength !== 0) {
-      this.expFirstClear(expLength);
-    } else if (operand === AppComponent.BACKSPACE && this.queueOperations.length === 0) {
+    } else if (operand === AppComponent.BACKSPACE &&
+      this.queueOperations.length === 0 &&
+      this.operation !== '' &&
+      this.operation !== '<-') {
       this.queueOperations.push(this.operation);
     } else if (operand === AppComponent.PLUS_MINUS) {
       this.unaryOperations(operand);
     } else {
-      if (this.queueOperations.length !== 0) {
-        this.expQueueOperations(operand);
+      if (this.queueOperations.length !== 0 &&
+        this.operation !== '+' && this.operation !== '-' && this.operation !== '*' && this.operation !== '/') {
+          this.expQueueOperations(operand);
       } else {
-        if (expLength === 2 && opLength !== 0) {
-          this.calc();
-        }
-        this.operation = operand;
-        if (this.result !== 0 && (operand in this.singleNumberOperation || operand in this.powerOperation)) {
-          this.calc();
+        if (operand !== '<-') {
+          if (expLength === 2 && opLength !== 0) {
+            this.calc();
+          }
+          this.operation = operand;
+          if (this.result !== 0 && (operand in this.singleNumberOperation || operand in this.powerOperation)) {
+            if (this.operation !== AppComponent.CE) {
+              this.calc();
+            }
+            if (this.operation === '=') {
+              this.operation = '';
+            }
+          }
         }
       }
     }
@@ -185,10 +242,10 @@ export class AppComponent {
     if (this.operation in this.singleNumberOperation || this.operation in this.powerOperation) {
       if (this.operation in this.singleNumberOperation) {
         this.temp = this.singleNumberOperation[this.operation]();
-        this.operation = '';
       } else if (this.operation in this.powerOperation) {
         this.temp = this.powerOperation[this.operation](this.expression[this.expression.length - 1]);
       }
+      this.operation = '';
       this.expression[0] = this.temp;
       this.result = this.temp;
       this.numberFlag = true;
@@ -199,6 +256,19 @@ export class AppComponent {
     // tslint:disable-next-line:triple-equals use-isnan
     if (this.temp == Infinity || isNaN(this.temp)) {
       this.result = this.temp;
+      // tslint:disable-next-line:triple-equals
+      if (this.temp == Infinity) {
+        setTimeout(() => {
+          this.result = 'Cannot divide by zero‬';
+          this.operation = '';
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          this.result = 'Result is undefined‬';
+          this.operation = '';
+          this.expression.length = 0;
+        }, 1000);
+      }
       this.result = 0;
       this.temp = 0;
       this.expression.length = 0;
@@ -213,9 +283,18 @@ export class AppComponent {
       this.expression.push(num);
       this.numberFlag = true;
     } else {
-      const lastElementLength = this.expression.length - 1;
-      this.expression[lastElementLength] = this.expression[lastElementLength] + num;
-      this.result = this.expression[lastElementLength];
+      if (this.expression.length !== 0) {
+        const lastElementLength = this.expression.length - 1;
+        if (this.expression[lastElementLength][0] === '0' && !this.expression[lastElementLength].indexOf('.')) {
+          this.expression[lastElementLength] = num;
+        } else {
+          this.expression[lastElementLength] = this.expression[lastElementLength] + num;
+        }
+        this.result = this.expression[lastElementLength];
+      }
+    }
+    if (this.operation === '<-') {
+      this.operation = '';
     }
   }
 
@@ -237,7 +316,7 @@ export class AppComponent {
     const exp = $event.target.innerText;
     // search "." or number or operand
     if (AppComponent.POINT === exp) {
-      this.addPoint(exp);
+        this.addPoint(exp);
     } else if (isNaN(+exp)) {
       this.addOperation(exp);
     } else {
